89cfafa8fc4a014cd8f2a8696baaa702df87483d
im linux <imlinux68@gmail.com>
2023-04-27 01:06:19 +0300

    created vpc

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@
 provider "aws" {
   region = "us-west-2"
 }
+
+# Create VPC
+
+resource "aws_vpc" "mtc_vpc" {
+  cidr_block = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = {
+    "Name" = "mtc_vpc"
+  }
+}
