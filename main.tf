0ada4467e850052409058002b58792bbb303beb6
im linux <imlinux68@gmail.com>
2023-04-27 09:03:27 +0300

    added route gate way

--- main.tf
+++ main.tf
@@ -27,10 +27,23 @@ resource "aws_internet_gateway" "mtc_internet_gateway" {
   }
 }
 
+resource "aws_route_table" "mtc_public_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
 
+  tags = {
+    "Name" = "mtc_pub-${random_id.random.dec}"   
+  }
+  
+}
+
+resource "aws_route" "default_route" {
+  route_table_id = aws_route_table.mtc_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+}
 
 resource "random_id" "random" {
-  byte_length = 2
+  byte_length = 5
 }
 
 
