18138c8fdace22c46584b2d9cf078efd73156786
im linux <imlinux68@gmail.com>
2023-04-27 08:28:33 +0300

    added igw

--- main.tf
+++ main.tf
@@ -7,11 +7,19 @@ provider "aws" {
 # Create VPC
 
 resource "aws_vpc" "mtc_vpc" {
-  cidr_block = "10.123.0.0/16"
+  cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {
     "Name" = "mtc_vpc"
+  }
+}
+
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    "Name" = "mtc_igw"
   }
 }
