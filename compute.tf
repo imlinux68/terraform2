e7aa085d5ebb44a12a8d0ceb1b67f63586bffad9
im linux <imlinux68@gmail.com>
2023-04-28 23:26:20 +0300

    added ec2 without key name

--- compute.tf
+++ compute.tf
@@ -8,3 +8,18 @@ data "aws_ami" "server_ami" {
     values = ["amzn2-ami-kernel-5.10-hvm-2.0.20230418.0-x86_64-gp2"]
   }
 }
+
+resource "aws_instance" "mtc_main" {
+  instance_type = var.main_instance_type
+  ami = data.aws_ami.server_ami.id
+  key_name = ""
+  vpc_security_group_ids = [aws_security_group.mtc_sg1.id]
+  subnet_id = aws_subnet.mtc_public_subnet[0].vpc_id
+  root_block_device {
+    volume_size = var.main_vol_size
+  }
+
+  tags = {
+    "Name" = "My mtc EC2"
+  }
+}
