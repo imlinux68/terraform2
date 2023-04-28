3f904eae0d14c9120ac7d5fe2397d3e13c6f297c
im linux <imlinux68@gmail.com>
2023-04-29 02:02:02 +0300

    added random instance with counter

--- compute.tf
+++ compute.tf
@@ -9,17 +9,23 @@ data "aws_ami" "server_ami" {
   }
 }
 
+resource "random_id" "mtc_node_id" {
+    byte_length = 2
+    count = var.main_instance_count
+}
+
 resource "aws_instance" "mtc_main" {
+  count = var.main_instance_count
   instance_type = var.main_instance_type
   ami = data.aws_ami.server_ami.id
-  key_name = ""
+  # key_name = ""
   vpc_security_group_ids = [aws_security_group.mtc_sg1.id]
-  subnet_id = aws_subnet.mtc_public_subnet[0].vpc_id
+  subnet_id = aws_subnet.mtc_public_subnet[count.index].id
   root_block_device {
     volume_size = var.main_vol_size
   }
 
   tags = {
-    "Name" = "My mtc EC2"
+    "Name" = "mtc-main-${random_id.mtc_node_id[count.index].dec}"
   }
 }
